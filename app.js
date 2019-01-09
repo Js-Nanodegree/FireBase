a790816da44ebea2648b43e6f03e592c622ee217
JS-Nanodegree <45201264+Js-Nanodegree@users.noreply.github.com>
2019-01-10 01:21:50 +0500

    crypto hashing

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var crypto = require('crypto')
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+let algorithm = 'aes-256-ecb',
+    password = '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+    iv = '60iP0h6vJoEa',
+    sol ='fhsafhdsjakGd'
+    var ivstring = iv.toString('hex').slice(0, 16)
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+function hash(phrase,sol)
+{
+var m = crypto.createHmac('sha512',sol)
+        .update(phrase)
+        .digest('hex')
+return (m)
+}
+
+function encrypt(text,sol) {
+    var cipher = crypto.createCipheriv(algorithm, password, sol)
+    var encrypted = cipher.update(text, 'utf8', 'hex')
+    encrypted += cipher.final('hex');
+    var tag = cipher.getAuthTag(sol);
+    return {
+      content: encrypted,
+      tag: tag
+    };
+  }
+
+  function decrypt(encrypted) {
+    var decipher = crypto.createDecipheriv(algorithm, password, sol)
+        decipher.setAuthTag(encrypted.tag);
+    var dec = decipher.update(encrypted.content, 'hex', 'utf8')
+        dec += decipher.final('utf8');
+    return dec;
+  }
+
+var z= hash(password,sol)
+  console.log('sha512 metosd hex '+ z)
+var hw = encrypt(z,sol)
+    console.log(hw)
+var mv =decrypt(hw)
+    console.log('Расшифровшик ' + mv);
+
+const buf = Buffer.from(sol);
+
+console.log(crypto.randomFillSync(buf).toString('hex'));
+
+crypto.randomFillSync(buf, 8);
+console.log(buf.toString('hex'));
+
+// The above is equivalent to the following:
+crypto.randomFillSync(buf, 8, 2);
+console.log(buf.toString('hex'));
