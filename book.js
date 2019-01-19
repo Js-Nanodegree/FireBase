a805c10ea7f64555a6786e9e1961e796fe3758e8
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-19 18:55:39 +0500

    события

--- book.js
+++ book.js
@@ -1,343 +1,105 @@
-var a = 1
-const b ='Rerd'
-let c = {b:a}
-// const func = () =>{try{ console.log('добавь данных')}catch(e){console.log('ошибка')}} 
-const fun = (c) =>{ return (c) } 
-console.log(fun(c))
-const car ={
-    model:'Ferra',
-    color:'Ford',
-    size:'M',
-    FullName: function(){
-        return `${this.color} ${this.model}`
-    }
-}
-const numbers = [1, "dsadas", 3, 4, 5]
-const sum = (a, b, c, d, e) => a + b + c + d + e
-const sum = sum(...numbers)
+Clipboard
 
-const items = { first, second, ...others }
-items 
+onCopy
+onCut
+onPaste
+Composition
+onCompositionEnd
+onCompositionStart
+onCompositionUpdate
 
-const person = {
-  firstName: 'Tom',
-  lastName: 'Cruise',
-  actor: true,
-  age: 54 //made up
-}
-const { firstName: name, age } = person
+Keyboard
 
+onKeyDown
+onKeyPress
+onKeyUp
 
-class Person {
-    constructor(name) {
-      this.name = name
-    }
-    hello() {
-      return 'Hello, I am ' + this.name + '.'
-    }
-  }
+Focus
 
-  const flavio = new Person('Flavio')
-flavio.hello()
+onFocus
+onBlur
 
-class Programmer extends Person {
-    hello() {
-      return super.hello() + ' I am a programmer.'
-    }
-  }
-  const flavio = new Programmer('Flavio')
-  flavio.hello()
+Form
 
-  class Person {
-    static genericHello() {
-      return 'Hello'
-    }
-  }
-  Person.genericHello() 
+onChange
+onInput
+onSubmit
 
-  class Person {
-    constructor(name) {
-      this.name = name
-    }
-    set name(value) {
-      this.name = value
-    }
-    get name() {
-      return this.name
-    }
-  }
+Mouse
 
-  class Person {
-    constructor(name) {
-      this.name = name
-    }
-    get name() {
-      return this.name
-    }
-  }
+onClick
+onContextMenu
+onDoubleClick
+onDrag
+onDragEnd
+onDragEnter
+onDragExit
+onDragLeave
+onDragOver
+onDragStart
+onDrop
+onMouseDown
+onMouseEnter
+onMouseLeave
+onMouseMove
+onMouseOut
+onMouseOver
+onMouseUp
+Selection
+onSelect
 
-  class Person {
-    constructor(name) {
-      this.name = name
-    }
-    set name(value) {
-      this.name = value
-    }
-  }
+Touch
 
-  const myFirstPromise = new Promise((resolve, reject) => {
-    // выполняется асинхронная операция, которая в итоге вызовет:
-    //
-    //   resolve(someValue); // успешное завершение
-    // или
-    //   reject("failure reason"); // неудача
-  });
+onTouchCancel
+onTouchEnd
+onTouchMove
+onTouchStart
 
-  const isItDoneYet = new Promise()
-//...
-const checkIfItsDone = () => {
-  isItDoneYet
-    .then(ok => {
-      console.log(ok)
-    })
-    .catch(err => {
-      console.error(err)
-    })
-}
+UI
 
-const f1 = fetch('/something.json')
-const f2 = fetch('/something2.json')
-Promise.all([f1, f2])
-  .then(res => {
-    console.log('Array of results', res)
-  })
-  .catch(err => {
-    console.error(err)
-  })
+onScroll
 
-  const promiseOne = new Promise((resolve, reject) => {
-    setTimeout(resolve, 500, 'one')
-  })
-  const promiseTwo = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, 'two')
-  })
-  Promise.race([promiseOne, promiseTwo]).then(result => {
-    console.log(result) // 'two'
-  })
+Mouse Wheel
 
-Promise.all([f1, f2]).then(([res1, res2]) => {
-    console.log('Results', res1, res2)
-  })
+onWheel
 
+Media
 
+onAbort
+onCanPlay
+onCanPlayThrough
+onDurationChange
+onEmptied
+onEncrypted
+onEnded
+onError
+onLoadedData
+onLoadedMetadata
+onLoadStart
+onPause
+onPlay
+onPlaying
+onProgress
+onRateChange
+onSeeked
+onSeeking
+onStalled
+onSuspend
+onTimeUpdate
+onVolumeChange
+onWaiting
 
-  const doSomethingAsync = () => {
-    return new Promise(resolve => {
-      setTimeout(() => resolve('I did something'), 3000)
-    })
-  }
+Image
 
-  const doSomething = async () => {
-    console.log(await doSomethingAsync())
-  }
+onLoad
+onError
 
-  const doSomethingAsync = () => {
-    return new Promise(resolve => {
-      setTimeout(() => resolve('I did something'), 3000)
-    })
-  }
-  const doSomething = async () => {
-    console.log(await doSomethingAsync())
-  }
-  console.log('Before')
-  doSomething()
-  console.log('After')
+Animation
 
-  const aFunction = async () => {
-    return 'test'
-  }
-  aFunction().then(alert) // This will alert 'test'
+onAnimationStart
+onAnimationEnd
+onAnimationIteration
 
-  const aFunction = async () => {
-    return Promise.resolve('test')
-  }
-  aFunction().then(alert) // This will alert 'test'
+Transition
 
-  const list = ['Apple', 'Orange', 'Egg']
-list.map(item => item[0]).filter(item => item === 'A') //'A'
-
-const Button = props => {
-    return <button>{props.text}</button>
-  }
-  const SubmitButton = () => {
-    return <Button text="Submit" />
-  }
-  const LoginButton = () => {
-    return <Button text="Login" />
-  }
-
-  const Button = props => {
-  return <button onClick={props.onClickHandler}>{props.text}</button>
-}
-const LoginButton = props => {
-  return <Button text="Login" onClickHandler={props.onClickHandler} />
-}
-const Container = () => {
-  const onClickHandler = () => {
-    alert('clicked')
-  }
-  return <LoginButton onClickHandler={onClickHandler} />
-}
-
-const Sidebar = props => {
-    return <aside>{props.children}</aside>
-  }
-
-  <Sidebar>
-  <Link title="First link" />
-  <Link title="Second link" />
-</Sidebar>
-
-const elements = ['one', 'two', 'three'];
-return (
-  <ul>
-    {elements.map((value, index) => {
-      return <li key={index}>{value}</li>
-    })}
-  </ul>
-)
-
-class BlogPostExcerpt extends Component {
-    constructor(props) {
-      super(props)
-      this.state = { clicked: false }
-    }
-    render() {
-      return (
-        <div>
-          <h1>Title</h1>
-          <p>Description</p>
-        </div>
-      )
-    }
-  }
-
-  class BlogPostExcerpt extends Component {
-    constructor(props) {
-      super(props)
-      this.state = { clicked: false }
-    }
-    render() {
-      return (
-        <div>
-          <h1>Title</h1>
-          <p>Description</p>
-          <p>Clicked: {this.setState({ clicked: true })}</p>
-        </div>
-      )
-    }
-  }
-
-  class Converter extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { currency: '€' }
-    }
-    handleChangeCurrency = event => {
-      this.setState({ currency: this.state.currency === '€' ? '$' : '€' })
-    }
-    render() {
-      return (
-        <div>
-          <Display currency={this.state.currency} />
-          <CurrencySwitcher
-            currency={this.state.currency}
-            handleChangeCurrency={this.handleChangeCurrency}
-          />
-        </div>
-      )
-    }
-  }
-  const CurrencySwitcher = props => {
-    return (
-      <button onClick={props.handleChangeCurrency}>
-        Current currency is {props.currency}. Change it!
-      </button>
-    )
-  }
-  const Display = props => {
-    return <p>Current currency is {props.currency}.</p>
-  }
-
-  import React, { Component } from 'react'
-  class BlogPostExcerpt extends Component {
-    render() {
-      return (
-        <div>
-          <h1>{this.props.title}</h1>
-          <p>{this.props.description}</p>
-        </div>
-      )
-    }
-  }
-
-  BlogPostExcerpt.propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string
-  }
-  BlogPostExcerpt.defaultProps = {
-    title: '',
-    description: ''
-  }
-
-  class UsersContainer extends React.Component {
-    constructor() {
-      this.state = {
-        users: []
-      }
-    }
-    componentDidMount() {
-      axios.get('/users').then(users =>
-        this.setState({ users: users }))
-      
-    }
-    render() {
-      return <Users users={this.state.users} />
-    }
-  }
-
-  PropTypes.arrayOf(PropTypes.string).isRequired,
-PropTypes.string.isRequired,
-
-PropTypes.shape({
-    color: PropTypes.string,
-    fontSize: PropTypes.number
-  })
-
-  PropTypes.arrayOf(PropTypes.string)
-
-  PropTypes.instanceOf(Something)
-
-  PropTypes.oneOf(['Test1', 'Test2']),
-
-  PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ])
-  import PropTypes from 'prop-types'
-import React from 'react'
-class BlogPostExcerpt extends Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.description}</p>
-      </div>
-    )
-  }
-}
-BlogPostExcerpt.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string
-}
-export default BlogPostExcerpt
+onTransitionEnd
