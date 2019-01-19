bdc11bf6016c62a80b7cff1eb309907e4845e572
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-19 17:01:33 +0500

    Разрушение объектов и массивов

---
+++ book.js
@@ -0,0 +1,32 @@
+var a = 1
+const b ='Rerd'
+let c = {b:a}
+// const func = () =>{try{ console.log('добавь данных')}catch(e){console.log('ошибка')}} 
+const fun = (c) =>{ return (c) } 
+console.log(fun(c))
+const car ={
+    model:'Ferra',
+    color:'Ford',
+    size:'M',
+    FullName: function(){
+        return `${this.color} ${this.model}`
+    }
+}
+const numbers = [1, "dsadas", 3, 4, 5]
+const sum = (a, b, c, d, e) => a + b + c + d + e
+const sum = sum(...numbers)
+
+const items = { first, second, ...others }
+items 
+
+const person = {
+    firstName: 'Tom',
+    lastName: 'Cruise',
+    actor: true,
+    age: 54 //made up
+  }
+  const { firstName: name, age } = person
+
+let z =mc
+console.log(z)
+
