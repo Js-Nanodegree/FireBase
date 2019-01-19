564f441701fdcb5fcad6c9cccc472711d0930a76
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-19 18:40:13 +0500

    кнопка нажатие

--- book.js
+++ book.js
@@ -237,3 +237,34 @@ class BlogPostExcerpt extends Component {
       )
     }
   }
+
+  class Converter extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { currency: '€' }
+    }
+    handleChangeCurrency = event => {
+      this.setState({ currency: this.state.currency === '€' ? '$' : '€' })
+    }
+    render() {
+      return (
+        <div>
+          <Display currency={this.state.currency} />
+          <CurrencySwitcher
+            currency={this.state.currency}
+            handleChangeCurrency={this.handleChangeCurrency}
+          />
+        </div>
+      )
+    }
+  }
+  const CurrencySwitcher = props => {
+    return (
+      <button onClick={props.handleChangeCurrency}>
+        Current currency is {props.currency}. Change it!
+      </button>
+    )
+  }
+  const Display = props => {
+    return <p>Current currency is {props.currency}.</p>
+  }
